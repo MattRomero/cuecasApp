0fada6ab33bce1595b6a4febbdcae19a8f9ea777
Matias Romero <mromero.olivos@gmail.com>
2020-03-06 21:47:05 -0300

    using api v2

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Post(props) {
-    return <p>{props.title}</p>
+    return <p>{props.title._text}</p>
 }
 
 export default Post;
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Post from './Post';
+import * as convert from 'xml-js';
 
 let APIKey = 'AIzaSyATJGT2-yS4h35iV9akdb4ycNvMNBALafM';
 
@@ -22,25 +23,32 @@ class PostList extends React.Component {
 
     getAllPosts = async (id,apikey) => {
         let data = [];
-        let targetUrl = `https://www.googleapis.com/blogger/v3/blogs/${id}/posts?key=${apikey}`;
+        
+        let targetUrl = `https://cors-anywhere.herokuapp.com/https://www.blogger.com/feeds/${id}/posts/default/?key=${apikey}&max-results=5`;
         let blob = await fetch(targetUrl);
-        let items = await blob.json();
-        for (let item of items.items) {
-            data.push(item);
+        let xmlCuecas = await blob.text();
+        let jsonCuecas = convert.xml2json(xmlCuecas, {compact: true});
+        jsonCuecas = JSON.parse(jsonCuecas);
+        jsonCuecas = jsonCuecas.feed;
+        
+        for (let item of jsonCuecas.entry) {
+            data.push(item)
         }
-        console.log(data)
-        return items;
+
+        return data;
     }
 
     render() {
         if (this.state.data) {
+        
             return (
-                <div>
-                    {this.state.data.items.map(post => (
+                <div> 
+                    {this.state.data.map(post => (
                         <Post title={post.title} />
                     ))}
                 </div>
             );
+
         }
         else {
             return (
